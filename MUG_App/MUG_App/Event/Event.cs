dbf5c59d54de3d2c73e1d80418f4138786292940
Thomas Kaelin <thomas.kaelin@bbv.ch>
2017-04-25 17:05:11 +0200

    VM: adopted Event to ViewModelBase.

--- MUG_App/MUG_App/Event/Event.cs
+++ MUG_App/MUG_App/Event/Event.cs
@@ -1,15 +1,11 @@
-﻿using System.ComponentModel;
-
-namespace MUG_App.Event
+﻿namespace MUG_App.Event
 {
-    public partial class EventPageViewModel : INotifyPropertyChanged
+    public class Event
     {
-        public class Event
-        {
-            public string Title { get; set; }
-            public string Description { get; set; }
+        public string Title { get; set; }
 
-            public override string ToString() => Title;
-        }
+        public string Description { get; set; }
+
+        public override string ToString() => $"{Title}: {Description}";
     }
 }
