942a0f298be8ae9eb609660d4b813a5f0a9547fd
Thomas Kaelin <thomas.kaelin@bbv.ch>
2017-04-25 17:33:55 +0200

    VM: adopted MainPage to ViewModelBase.

--- MUG_App/MUG_App/Main/MainPageMasterViewModel.cs
+++ MUG_App/MUG_App/Main/MainPageMasterViewModel.cs
@@ -1,29 +1,20 @@
 ﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using Xamarin.Forms;
+using MUG_App.Common;
 
 namespace MUG_App.Main
 {
-    public partial class MainPageMaster : ContentPage
+    public class MainPageMasterViewModel : ViewModelBase
     {
-        class MainPageMasterViewModel : INotifyPropertyChanged
+        public MainPageMasterViewModel()
         {
-            public ObservableCollection<MainPageMenuItem> MenuItems { get; }
-            public MainPageMasterViewModel()
+            MenuItems = new ObservableCollection<MainPageMenuItem>(new[]
             {
-                MenuItems = new ObservableCollection<MainPageMenuItem>(new[]
-                {
-                    new MainPageMenuItem { Id = 0, Title = "Group" },
-                    new MainPageMenuItem { Id = 1, Title = "Organizers" },
-                    new MainPageMenuItem { Id = 2, Title = "Events" },
-                });
-            }
-            public event PropertyChangedEventHandler PropertyChanged;
-            void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-
+                new MainPageMenuItem { Id = 0, Title = "Group" },
+                new MainPageMenuItem { Id = 1, Title = "Organizers" },
+                new MainPageMenuItem { Id = 2, Title = "Events" },
+            });
         }
+
+        public ObservableCollection<MainPageMenuItem> MenuItems { get; }
     }
 }
